a5b798d3209008eba49d9e867cba6c7d9b6ac7b1
Nathan <nathanwestrope@gmail.com>
2022-02-18 13:12:59 -0600

    updated files

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 
-int main ( )
+/**
+ * @brief 
+ * 
+ * @parm fib
+ * @return int fib
+ */
+
+int fib(int n);
+int main ()
 {
+    
     std::cout << "Hello, World!" << std::endl;
+    for (int i{0}; i < 6; ++i)
+    {
+    std::cout << "fib(" << i << ")"<< fib(i) << std::endl;
+    }
     return 0;
+}
+
+int fib(int n)
+{
+    if  (n == 0 or n == 1)
+    {
+        //base case
+        return n; 
+    }
+
+    return  n - fib(n-1) + fib(n - 2);
 }
